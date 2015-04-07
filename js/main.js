f8c48ae70580477db3f1e6a3a00ad477e3a46a10
boris <borislav.arc@gmail.com>
2015-04-07 09:33:03 +0300

    smart-7

--- js/main.js
+++ js/main.js
@@ -6,22 +6,47 @@
     }, 300);
 
 
-    // Add Click Event on Images
+    // Add Events on Images
     var items = document.querySelectorAll('.item');
 
     for(var i = 0; i < items.length; i++) {
-        items[i].addEventListener('click', function(){
-            this.parentNode.parentNode.classList.toggle('next');
-        });
+        items[i].addEventListener('click', clickItem);
+        items[i].addEventListener('mouseover', hoverItem);
+        items[i].addEventListener('mouseout', mouseOutItem);
     }
 
-    $('.item').hover(function() {
-        $(this).addClass('hover')
-    }, function() {
-        var $this = $(this);
+    // Click on Item
+    function clickItem(){
+        this.parentNode.parentNode.classList.toggle('next');
+    }
 
-        //setTimeout(function(){
-        $(this).removeClass('hover');
-        //},300)
-    })
+    // Hover on Item
+    function hoverItem() {
+        console.log('1');
+        this.classList.add('hover')
+    }
+
+    // Mouse out from Item
+    function mouseOutItem() {
+        console.log('2');
+        this.classList.remove('hover')
+    }
+
+    // Check IE or old browsers for css property - transform: preserve3d
+    Modernizr.addTest('csstransformspreserve3d', function () {
+        var prop, val, cssText, ret;
+
+        prop = 'transform-style';
+        if ('webkitTransformStyle' in document.documentElement.style) {
+            prop = '-webkit-' + prop;
+        }
+        val = 'preserve-3d';
+        cssText = '#modernizr { ' + prop + ': ' + val + '; }';
+
+        Modernizr.testStyles(cssText, function (el, rule) {
+            ret = window.getComputedStyle ? getComputedStyle(el, null).getPropertyValue(prop) : '';
+        });
+
+        return (ret === val);
+    });
 })();
